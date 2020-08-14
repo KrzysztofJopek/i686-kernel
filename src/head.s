edcf3de286192b90d02c64d402926e7f72dc616e
Krzysztof Jopek <krzysztofjopek99@gmail.com>
2020-08-14 14:43:17 +0000

    Enable paging, move kernel to higher half

--- src/head.s
+++ src/head.s
@@ -11,42 +11,79 @@ MB_MAGIC equ 0x1BADB002
 MB_FLAGS equ (1<<0)|(1<<1)
 MB_CHECKSUM equ (0 - (MB_MAGIC + MB_FLAGS))
 
-section .multiboot
+section .multiboot.data
         ALIGN 4
         dd MB_MAGIC
         dd MB_FLAGS
         dd MB_CHECKSUM
 
+PG_KERN 	equ 0xC0000000
+PG_KERN_POS 	equ (PG_KERN >> 22)
 section .bss
-	align 16
+	align 4096
 	stack_bottom:
-	resb 8196
+	resb 8192
 	stack_top:
+	pgdir:
+	resb 4096
+	pgtab:
+	resb 4096
 
-section .data
-	mem_ptr dd 0
+;	mem_ptr dd 0
 
 
-section .text
+PG_TAB_NEXT_VAL equ (1 << 12)
+section .multiboot.text
 	_start:
 		cli ; disable interrupts
-		mov [mem_ptr], ebx
+		mov edx, ebx
 
 		;clear segments
 		xor ax, ax
 		mov ds, ax
 		mov es, ax
 
+		;setup pgdir
+		mov eax, (pgtab - PG_KERN)
+		mov ebx, (pgdir - PG_KERN)
+		;map entry for 0xC00
+		mov [ebx + PG_KERN_POS*4], eax
+		or  [ebx + PG_KERN_POS*4], dword 0x3
+		;map entry for 0x0
+		mov [ebx], eax
+		or  [ebx], dword 0x3
+
+		;setup page table
+		mov ecx, eax
+		add ecx, dword 4096
+		mov ebx, dword 0x3
+		next_page:
+		mov [eax], ebx
+		add ebx, PG_TAB_NEXT_VAL
+		add eax, dword 0x4
+		cmp eax, ecx
+		jl next_page
+
+		;enable paging
+		mov eax, (pgdir - PG_KERN)
+		mov cr3, eax
+		mov eax, cr0
+		or eax, dword 0x80000001
+		mov cr0, eax
+		nop
+		lea ecx, call_main
+		jmp ecx
+
+section .text
+		call_main:
 		call set_gdt
 
-	
 		mov esp, stack_top
-		mov ebx, [mem_ptr]
+		mov ebx, edx
 		push ebx
 		call kmain
 		hlt
 
-		
 		;enable a20
 		;not used -- multiboot
 	enable_a20:
@@ -82,8 +119,6 @@ section .text
 		mov fs, ax
 		mov gs, ax
 		ret
-
-
 
 	keyboard_handler:
 		call    keyboard_handler_main
